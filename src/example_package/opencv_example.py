7158a906decc2595bc09d0c3212e673a97ac6a2d
Damiano Salvitti <damianosalvitti@gmail.com>
2022-05-04 16:14:31 +0200

    Prime modifiche opencv_example

--- src/example_package/opencv_example.py
+++ src/example_package/opencv_example.py
@@ -1,2 +1,11 @@
+import cv2
+
+print('1 ok')
+import numpy as np
+
+eye = np.eye(3, dtype="uint8")
+print(eye)
+'''
 if __name__ == '__main__':
     print('Stai eseguendo il file "opencv_example.py"!\n')
+'''
