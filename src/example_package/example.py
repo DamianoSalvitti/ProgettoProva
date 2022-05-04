189d7cee5ee7604140441e08980579d4fa9db18b
Damiano Salvitti <damianosalvitti@gmail.com>
2022-05-04 09:19:01 +0200

    Test simple_run verificato correttament, aggiornamento example, aggiunta gitignore

--- src/example_package/example.py
+++ src/example_package/example.py
@@ -1,6 +1,9 @@
 def add_one(number):
     return number + 1
 
-if __name__ == __main__:
+if __name__ == '__main__':
+    print('"Hello from example" come main!\n')
+    print('Qui sotto viene stampata una lista di numeri:\n')
     for i in range(1,13,2):
         print(add_one(i))
+
--- tests/simple_run.py
+++ tests/simple_run.py
@@ -1,9 +1,17 @@
-from example_package import example
+import sys, os
+f_path = os.path.dirname(__file__)
+parent_path = os.path.join(f_path, '..')
+sys.path.append(os.path.abspath(parent_path))
+
+from src.example_package import example
 
 def x_times_y (x, y):
     return x*y
 
-a = example.add(5)
-c = x_times_y(a,2)
+if __name__ == '__main__':
+    a = example.add_one(5)
+    c = x_times_y(a,2)
 
-print(c)
+    print('Modulo "example" importato correttamente.')
+    print('"Hello from simple_run" come main!\n')
+    print(f'Il risultato e\': {c}.')
