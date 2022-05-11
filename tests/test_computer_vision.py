a1fd5300ea41477cc4386afb99b56b1be44980ba
Damiano Salvitti <damianosalvitti@gmail.com>
2022-05-11 12:39:48 +0200

    Execution test for PYTHONPATH

--- tests/test_computer_vision.py
+++ tests/test_computer_vision.py
@@ -1,11 +1,11 @@
 import sys
 import os
-
+'''
 # Reach the parent's path
 f_path = os.path.dirname(__file__)
 root_path = os.path.join(f_path, os.pardir)
 sys.path.append(os.path.abspath(root_path))
-
+'''
 # Import of the desired module
 import src.compute_flask_cv.computer_vision as cvn
 
